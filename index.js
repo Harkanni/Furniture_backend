0fc85bc5d656f118ad14e88035f55dba44d28af6
harkanni <harkanzyempire001@gmail.com>
2023-11-21 16:34:14 +0100

    nodemon

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import router from './routes/route.js';
+import cors from 'cors'
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json({ limit: '10mb'}));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
@@ -18,8 +20,6 @@ app.use('/api/v1/products', router)
 mongoose.connect(process.env.MONGO_URL)
     .then(() => app.listen(PORT, () => console.log(`Server runnning on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
-   
-
 
 
 
